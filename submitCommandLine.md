778ff5bfe31408cf6a3c6cbce3d6013ecabab9c4
jeness <yuhaoran9968@gmail.com>
2018-09-30 16:46:35 -0400

    finish task 2

--- submitCommandLine.md
+++ submitCommandLine.md
@@ -1,6 +1,9 @@
 1. show content
 gsutil cat gs://ufcloudcomputing/wordCountTwo.py
 2. submit to run
++ task1
+use web ui to submit
++ task2
 gcloud dataproc jobs submit pyspark \
     --cluster cluster-assign1 --region global \
     gs://ufcloudcomputing/wordCountTwo.py
--- wordCountTwo.py
+++ wordCountTwo.py
@@ -1,6 +1,29 @@
-#!/usr/bin/python
-import pyspark
-sc = pyspark.SparkContext()
-rdd = sc.parallelize(['Hello,', 'world!'])
-words = sorted(rdd.collect())
-print(words)
+from __future__ import print_function
+
+from operator import add
+import sys
+from pyspark import SparkContext
+
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="PythonWordCountTwo")
+    lines = sc.textFile("gs://ufcloudcomputing/bible", 1)
+  
+    def pairs(param):
+        tempList = param.split(" ")
+        index = 0 
+        start = 0
+        dup = []
+        for word in tempList:
+            if(start == 0):
+                start = 1
+                index+=1
+            else:
+                dup.append((str(tempList[index-1]),str(word)))
+                index+=1
+        return dup
+ 
+    counts = lines.flatMap(pairs).map(lambda x: (str(x),1)).reduceByKey(add)
+    counts.saveAsTextFile("gs://ufcloudcomputing/wordcountTwoOutputFolder/")
+
+    sc.stop()
